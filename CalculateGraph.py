65c781cf3d583af5d92e187c0770d62d022c866b
Alan-First <1968472551@qq.com>
2022-03-31 20:26:19 +0800

    graph add description

--- CalculateGraph.py
+++ CalculateGraph.py
@@ -4,13 +4,14 @@ Version:
 Autor: Hedwig
 Date: 2022-03-31 16:25:56
 LastEditors: Hedwig
-LastEditTime: 2022-03-31 19:02:47
+LastEditTime: 2022-03-31 20:00:45
 '''
 from Graph import Node,Graph
 
 graph = Graph()
 
 # 创建一个继承节点类的运算类作为后续操作的父类
+# 类本身想要前向和后向操作
 class Function(Node):
     def forward(self,*argv):
         pass
@@ -18,36 +19,39 @@ class Function(Node):
     def backward(self,grad):
         pass
 
+# 创建张量类，张量类本身记录自身数据大小和梯度，同时还有前后向的节点信息
 class Tensor(Function):
     def __init__(self,data):
         super().__init__()#IndexGetter.get_id()
         self.data = data
         self.grad = 0
     
+    # 张量本身是一个数，不是函数，没有前向的说法
+    # 它的反向
     def backward(self,grad=1):
         self.grad += grad
         for previous_node in self.previous_nodes:
             previous_node.backward(grad)
 
 
+# 乘法类，前向就是两数相乘
 class Multiply(Function):
     def forward(self,x:Tensor,y:Tensor)->Tensor:
-        result_tensor = Tensor(x.data * y.data)
-        #print(self.id)
-        graph.add_edge(self,result_tensor)
+        result_tensor = Tensor(x.data * y.data)# 先做一次乘法
+        graph.add_edge(self,result_tensor)# 乘法操作与乘法结果为新的节点加入图中
         return result_tensor
 
+    # 反向就是求导，x*y求导，对x求导是y，对y求导是x
     def backward(self,grad):
-        x_node:Tensor = self.previous_nodes[0]
+        x_node:Tensor = self.previous_nodes[0]# 乘法的前驱节点0位置是x，因为multiply先添加的x
         y_node:Tensor = self.previous_nodes[1]
-        x_node.backward(grad * y_node.data)
+        x_node.backward(grad * y_node.data)# 由于乘法的链式求导法则，反向时候是累乘的
         y_node.backward(grad * x_node.data)
 
 def multiply(x:Tensor,y:Tensor):
     multiply_node = Multiply()#IndexGetter.get_id()
-    #print(multiply_node.id)
-    graph.add_edge(x,multiply_node)
-    graph.add_edge(y,multiply_node)
+    graph.add_edge(x,multiply_node)# 先添加的x
+    graph.add_edge(y,multiply_node)# 后添加的y
     return multiply_node.forward(x,y)
 
 class Add(Function):
--- Graph.py
+++ Graph.py
@@ -4,31 +4,39 @@ Version:
 Autor: Hedwig
 Date: 2022-03-31 16:11:17
 LastEditors: Hedwig
-LastEditTime: 2022-03-31 19:02:40
+LastEditTime: 2022-03-31 19:48:48
 '''
-
 # 用邻接表实现图
 # Node类对象记录每个节点，用列表记录节点的上一个节点和下一个节点
 # 本实现来自于张伟振的《深度学习原理与Pytorch实战》
 # 这部分主要是模拟pytorch张量的计算
 class Node:
-    def __init__(self,idx=0):
-        self.id = id(self)
-        self.previous_nodes = []
-        self.next_nodes = []
+    def __init__(self):
+        self.id = id(self)# 这个属性值作为区分对象的标志
+        self.previous_nodes = [] # 前驱节点
+        self.next_nodes = [] #后续节点
 
 # 先创建Node对象和Graph对象，用add_edge方法实现对左右节点的链接，这种链接时按键值对实现的
 # 字典的键是id，值是节点对象
 class Graph:
     def __init__(self):
         self.Nodes = {}
-    def add_edge(self,tail:Node,head:Node)->Node:
-        if tail.id not in self.Nodes:
-            self.Nodes[tail.id] = tail
+    def add_edge(self,head:Node,tail:Node)->Node:
+        """
+        @Params :要链接的两个节点head和tail
+        
+        @Description :将head指向tail，并将head和tail都存进有向图字典中，id:节点按键值对存储
+        
+        @Returns     :
+        """
+        
         if head.id not in self.Nodes:
             self.Nodes[head.id] = head
-        self.Nodes[tail.id].next_nodes.append(head)
-        self.Nodes[head.id].previous_nodes.append(tail)
+        if tail.id not in self.Nodes:
+            self.Nodes[tail.id] = tail
+        self.Nodes[head.id].next_nodes.append(tail)
+        self.Nodes[tail.id].previous_nodes.append(head)
+        
     def traverse(self):
         for node_id in self.Nodes:
             print("{}:{}".format(node_id,','.join([str(next_node.id) \
