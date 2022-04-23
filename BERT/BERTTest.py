080fb900e08297fb542ec40234c5399e066844d9
Alan-First <1968472551@qq.com>
2022-04-23 21:15:30 +0800

    construct bert for sentiment

--- BERT/BERTTest.py
+++ BERT/BERTTest.py
@@ -8,9 +8,11 @@
 @Version      :1.0
 '''
 
+from unittest.mock import sentinel
 from sklearn import preprocessing
 import torch
 from torch import dtype
+from torch import embedding
 import torch.nn as nn
 import torch.optim as optim
 from transformers import BertTokenizer,BertModel
@@ -21,7 +23,7 @@ import time
 
 
 SEED = 2022
-TRAIN = False
+TRAIN = True
 BATCH_SIZE=128
 N_EPOCHS=5
 HIDDEN_DIM=256
@@ -29,6 +31,7 @@ OUTPUT_DIM=1
 N_LAYERS=2
 BIDIRECTIONAL=True
 DROPOUT=0.25
+TEXT = 'I like you'
 
 random.seed(SEED)
 np.random.seed(SEED)
@@ -84,4 +87,111 @@ def load_data():
         )
     return train_iter,valid_iter,test_iter
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+bert_model = BertModel.from_pretrained('bert-base-uncased')
 
+# 情感分类任务在bert加上两层gru
+class SentimentModel(nn.Module):
+    def __init__(self,bert,hidden_dim,output_dim,n_layers,bidirectional,dropout) -> None:
+        super(SentimentModel,self).__init__()
+        self.bert = bert
+        embedding_dim = bert.config.to_dict()['hidden_size']
+        self.rnn = nn.GRU(
+            embedding_dim,
+            hidden_dim,num_layers=n_layers,
+            bidirectional=bidirectional,
+            batch_first=True,
+            dropout=0 if n_layers<2 else dropout
+            )
+        self.out = nn.Linear(hidden_dim*2 if bidirectional else hidden_dim,output_dim)
+        self.dropout = nn.Dropout(dropout)
+    def forward(self,text):
+        with torch.no_grad():
+            embedded = self.bert(text)[0]
+        _,hidden = self.rnn(embedded)
+        if self.rnn.bidirectional:
+            hidden = self.dropout(torch.cat((hidden[-2,:,:],hidden[-1,:,:]),dim=1))
+        else:
+            hidden = self.dropout(hidden[-1,:,:])
+        output = self.out(hidden)
+        return output
+
+model = SentimentModel(bert_model,HIDDEN_DIM,OUTPUT_DIM,N_LAYERS,BIDIRECTIONAL,DROPOUT)
+#print(model)
+
+def epoch_time(start_time,end_time):
+    elapsed_time = end_time-start_time
+    elapsed_mins = int(elapsed_time/60)
+    elapsed_secs = int(elapsed_time-(elapsed_mins*60))
+    return elapsed_mins,elapsed_secs
+
+def binary_accuracy(preds,y):
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds==y).float()
+    acc = correct.sum()/len(correct)
+    return acc
+
+def train(model,iterator,optimizer,criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+    model.train()
+    for batch in iterator:
+        optimizer.zero_grad()
+        predictions = model(batch.text).squeeze(1)
+        loss = criterion(predictions,batch.label)
+        acc = binary_accuracy(predictions,batch.label)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+    return epoch_loss/len(iterator),epoch_acc/len(iterator)
+
+def evaluate(model,iterator,criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+    model.eval()
+    with torch.no_grad():
+        for batch in iterator:
+            predictions = model(batch.text).squeeze(1)
+            loss = criterion(predictions,batch.label)
+            acc = binary_accuracy(predictions,batch.label)
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+    return epoch_loss/len(iterator),epoch_acc/len(iterator)
+
+def predict_sentiment(model,tokenizer,sentence):
+    model.eval()
+    tokens = tokenizer.tokenize(sentence)
+    tokens = tokens[:max_input_len-2]
+    indexed = [init_token_id]+tokenizer.convert_tokens_to_ids(tokens)+[eos_token_id]
+    tensor = torch.LongTensor(indexed).to(device)
+    tensor = tensor.unsqueeze(0)
+    prediction = torch.sigmoid(model(tensor))
+    return prediction.item()
+
+if __name__=='__main__':
+    if TRAIN:
+        train_iter,valid_iter,test_iter = load_data()
+        optimizer = optim.Adam(model.parameters())
+        criterion = nn.BCEWithLogitsLoss().to(device)
+        model = model.to(device)
+        best_val_loss = float('inf')
+        for epoch in range(N_EPOCHS):
+            start_time = time.time()
+            train_loss,train_acc = train(model,train_iter,optimizer,criterion)
+            valid_loss,valid_acc = evaluate(model,valid_iter,criterion)
+            end_time = time.time()
+            epoch_mins,epoch_secs = epoch_time(start_time,end_time)
+            if valid_loss<best_val_loss:
+                best_val_loss = valid_loss
+                torch.save(model.state_dict(),'model.pt')
+            print(f'Epoch:{epoch+1:02}|Epoch Time:{epoch_mins}m {epoch_secs}s')
+            print(f'\tTrain Loss:{train_loss:.3f}|Train Acc:{train_acc*100:.2f}%')
+            print(f'\t Val Loss:{valid_loss:.3f}| Val Acc:{valid_loss*100:.2f}%')
+        model.load_state_dict(torch.load('model.pt'))
+        test_loss,test_acc = evaluate(model,test_iter,criterion)
+        print(f'Test Loss:{test_loss:.3f}|Test Acc:{test_acc*100:.2f}%')
+    else:
+        model.load_state_dict(torch.load('model.pt',map_location=device))
+        sentiment = predict_sentiment(model,tokenizer,TEXT)
+        print(sentiment)
